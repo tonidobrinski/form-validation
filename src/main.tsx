2b5b8e567932320a26110a4b9935df8285670608
Toni Dobrinski <tdobrinski@abv.bg>
2025-05-15 22:47:16 +0300

    put index.scss in styles folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.scss";
+import "./styles/index.scss";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
